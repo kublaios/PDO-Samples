7948287af1ce6769e83f3d1bff084009bad06a17
Kubilay Erdogan MBP <kublaios@gmail.com>
2014-07-13 12:47:55 +0300

    Object class with constructor and record function, sample usage

--- index.php
+++ index.php
@@ -1,17 +1,11 @@
 <?php
 
-// Attempt to connect with credentials
-$con = mysql_connect("localhost", "root", "");
-if (!$con) {
-  die('Could not connect: ' . mysql_error());
-}
-if (!mysql_select_db("pdo_samples")) {
-  die('Could not select database: ' . mysql_error());
-}
+require_once __dir__."/Object.php";
 
-// Create SQL and run
-$sql = "SELECT * FROM `table` WHERE 1";
-mysql_query($sql,$con);
-
-// Close the connection
-mysql_close($con);
+$object = new Object();
+$object->name = "Hello PDO";
+if ($object->recordObject()) {
+  echo "New record with ID: ".$object->id;
+} else {
+  echo "Could not record";
+}
